2cd13f920b86aff454cfac3f6698eda43623120a
movibe <agfoccus@gmail.com>
2020-02-09 22:39:10 -0300

    chore: now deploy

--- src/rootNavigator.tsx
+++ src/rootNavigator.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { NavigationNativeContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { useTheme } from 'react-native-paper';
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import {DarkTheme, DefaultTheme, NavigationContainer} from '@react-navigation/native'
+import React from 'react'
+import {useTheme} from 'react-native-paper'
 
-import { StackNavigator } from './stack';
-import { DrawerContent } from './drawerContent';
+import {DrawerContent} from './drawerContent'
+import {StackNavigator} from './stack'
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
 export const RootNavigator = () => {
-  const theme = useTheme();
-  const navigationTheme = theme.dark ? DarkTheme : DefaultTheme;
+  const theme = useTheme()
+  const navigationTheme = theme.dark ? DarkTheme : DefaultTheme
 
   return (
-    <NavigationNativeContainer theme={navigationTheme}>
+    <NavigationContainer theme={navigationTheme}>
       <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={StackNavigator} />
       </Drawer.Navigator>
-    </NavigationNativeContainer>
-  );
-};
+    </NavigationContainer>
+  )
+}
